68d5954a3c8e2f70fef2099d082c3186a0ae7dba
J3xLe1988B3lx0x2E6 <73410627+J3xLe1988B3lx0x2E6@users.noreply.github.com>
2020-11-12 10:26:08 -0600

    Add files via upload

---
+++ comandos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Lista de comandos: "
+echo "help-----------------AYUDA"
+echo "inforsys ------------Informacion del sistema"
+echo "arbol----------------Muestra la jerarquia del directorio"
+echo "buscar---------------busca un archivo especifico"
+echo "fecha----------------Da la fecha"
+echo "hora-----------------Da la hora"
+echo "creaditos------------Muestra los creditos de los programadores"
+echo "piedra papel---------Juego de piedra, papel, tijeras, spok"
+echo "gato-----------------Juego de gato"
+echo "prebplayer-----------Biblioteca y reproduccion de musica"
+ 
